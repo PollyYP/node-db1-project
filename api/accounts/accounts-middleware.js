16774dccea119cd83b082ea9ebac63fc9d0a664b
Polly Yospan <porgroovy@gmail.com>
2021-06-16 23:38:22 -0400

    finish all middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,64 @@
+const db = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const name = req.body.name;
+  const budget = req.body.budget;
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  if (!name || !budget) {
+    return res.status(400).json({ message: "name and budget are required" });
+  }
+  if (typeof name !== "string") {
+    return res
+      .status(400)
+      .json({ message: "name of account must be a string" });
+  }
+  if (name.length < 3 || name.length > 100) {
+    return res
+      .status(400)
+      .json({ message: "name of account must be between 3 and 100" });
+  }
+  if (typeof budget !== "number") {
+    return res
+      .status(400)
+      .json({ message: "budget of account must be a number" });
+  }
+  if (budget < 0 || budget > 1000000) {
+    return res
+      .status(400)
+      .json({ message: "budget of account is too large or too small" });
+  }
+  next();
+};
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const allAccounts = await db.getAll();
+    allAccounts.map((account) => {
+      if (account.name === req.body.name) {
+        res.status(400).json({
+          message: "that name is taken",
+        });
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.checkAccountId = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const accountID = await db.getById(id);
+    if (!accountID) {
+      res.status(404).json({
+        message: "Account ID not found",
+      });
+    } else {
+      req.accountID = accountID;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
